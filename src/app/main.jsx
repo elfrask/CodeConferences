fcddba6a49075cd3bd253bdd3d56e525e9619fa6
elfrask <elfraskodev@gmail.com>
2022-05-13 21:48:45 -0400

    principal layer finished

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -4,7 +4,6 @@ let socket = io();
 
 
 
-
 class App extends React.Component {
 
     state = {
@@ -15,24 +14,56 @@ class App extends React.Component {
     };
 
     render() {
-
+        app_instanced = this;
 
         return (
             <div className="app">
                 <div className="head">
 
+                    
+                    
+                    
                     {(
                         this.state.join?(
                             // Solo si esta unido
                             this.state.master?(
                                 // Modo maestro
                                 <div>
+                                    {
+                                        [
+                                            ["Invitar", "/src/img/icons/guest.png"],
+                                            ["Cerrar la sala", "/src/img/icons/exit.png"],
+                                            ["Miembros", "/src/img/icons/group.png"]
+                                        ].map(x=>{
+                                            return(
 
+                                                <Bta portrait={{
+                                                    img:x[1]
+                                                }} landscape={{height:"50px"}} className="l" click={() => {console.log("Hola mundo uwu")}}>
+                                                    {x[0]}
+                                                </Bta>
+                                            )
+                                        })
+                                    }
                                 </div>
                             ):(
                                 // Modo invitado
                                 <div>
+                                    {
+                                        [
+                                            ["Salir de la sala", "/src/img/icons/exit.png"],
+                                            ["Miembros", "/src/img/icons/group.png"]
+                                        ].map(x=>{
+                                            return(
 
+                                                <Bta portrait={{
+                                                    img:x[1]
+                                                }} landscape={{height:"50px"}} className="l" click={() => {console.log("Hola mundo uwu")}}>
+                                                    {x[0]}
+                                                </Bta>
+                                            )
+                                        })
+                                    }
                                 </div>
                             )
 
@@ -44,7 +75,24 @@ class App extends React.Component {
                     <div className="r img" style={{
                         "width":"50px",
                         "height":"50px",
-                        "backgroundImage":`url("/src/info.png")`
+                        "backgroundImage":`url("/src/img/icons/info.png")`
+                    }} onClick={() => {
+                        show(
+                            <div>
+                                {
+                                    range(0, 20).map(x => {
+                                        return (
+                                            <h1>
+                                                Hola mundo
+                                            </h1>
+                                        )
+                                    }) 
+                                }
+                            </div>,
+                            () => {
+
+                            }
+                        )
                     }}>
 
                     </div>
@@ -52,10 +100,107 @@ class App extends React.Component {
                 </div>
                 <div className="body">
 
+                    {(
+                        this.state.join?(
+                            this.state.master?(
+                                []                        
+                            ):(
+                                []
+                            )    
+                        ):(
+                            <div className="medio fill">
+                                <div className="boxjoin">
+                                    <div className="headbox">
+                                        <div className="bthead_join medio" onClick={() => {this.setState({login:true})}}>
+                                            Unirme
+                                        </div>
+                                        <div className="bthead_join medio" onClick={() => {this.setState({login:false})}}>
+                                            Crear sala
+                                        </div>
+                                    </div>
+                                    <div className="boxbody medio">
+                                        {
+                                            this.state.login?(
+                                                // Unirme
+                                                <div>
+                                                    <center className="join_come">
+                                                        <input placeholder="Nombre" id="nombre" type="text" className="form_input_join" />
+                                                        <input placeholder="Codigo" id="Codigode invitacion" type="text" className="form_input_join" />
+                                                        <div className="sumit">
+                                                            Unirme
+                                                        </div>
+                                                    </center>
+                                                </div>
+                                            ):(
+                                                // Crear
+                                                <div>
+                                                    <center className="join_come">
+                                                        <input placeholder="Nombre" id="nombre" type="text" className="form_input_join" />
+                                                        <div className="sumit">
+                                                            Crear sala
+                                                        </div>
+                                                    </center>
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        )
+                    )}
+
+                </div>
+                <div className="over" id="over">
+
+                    <div className="over_windows">
+                        <div className="head">
+                            <div className="r img" style={{
+                                "width":"50px",
+                                "height":"50px",
+                                "backgroundImage":`url("/src/img/icons/close.png")`
+                            }} onClick={() => {
+
+                                let over = go("over");
+                                over.style.display = "none";
+
+                            }}>
+
+                            </div>
+                        </div>
+                        <div className="body over_content" id="over_content" style={{
+                            "padding":"20px",
+                            "width":"calc(100% - 40px)",
+                            "height":"calc(100% - 40px - 76px)",
+                        }}>
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
         );
     }
+}
+
+
+let app_instanced = (new App())
+
+
+function show(page, call) {
+    ReactDOM.render(
+        page,
+        go("over_content"),
+        () => {
+            
+            let over = go("over");
+                                
+            over.style.display = "flex";
+
+            genlink(call)();
+
+        }
+    )
 }
 
 
--- src/js/base.jsx
+++ src/js/base.jsx
@@ -1,5 +1,30 @@
 let go = (a) => document.getElementById(a);
 let asi = (a, b) => Object.assign(a,b);
+function range(i, f) {
+    let salida = [];
+    for (let x = i; x < f; x++) {
+        salida.push(x)
+        
+    }
+    return salida
+}
+function genlink(link) {
+    
+    switch (typeof(link)) {
+        case "string":
+            
+            return () => {document.location.assign(link)};
+        case "function":
+
+            return link
+    
+        default:
+            return () => {};
+            
+    }
+
+    
+}
 
 let verticalOrientation = false;
 
@@ -13,17 +38,44 @@ function getOrientation() {
 
 getOrientation()
 
+let list_Element_refresh = [];
+function auto_refresh(e) {
+    list_Element_refresh.push(e)
+}
+
 screen.orientation.addEventListener("change", () => {
     getOrientation()
+
+    list_Element_refresh.forEach(x=>{
+
+        x.setState({ori_v:getOrientation()})
+        console.log(x)
+
+    })
+
+    console.log("done")
 })
 
-class bta extends React.Component {
+class Bta extends React.Component {
+
+    state= {
+        ori_v:getOrientation()
+    }
 
     render() {
 
-        let ori_V = getOrientation();
-        let ori_V_img = this.props.portrait.img||""
-        let content = this.props.children
+        //this.setState
+
+        auto_refresh(this)
+
+        let ori_V = this.state.ori_v;
+        let ori_V_img = this.props.portrait.img||"";
+        let content = this.props.children;
+
+        if (ori_V) content = [];
+
+        this.props.portrait = this.props.portrait||{};
+        this.props.landscape = this.props.landscape||{};
 
         let ori_V_style = {
             width:this.props.portrait.width||"50px",
@@ -38,7 +90,7 @@ class bta extends React.Component {
         
 
         return (
-            <div className={`bta ${this.props.className||""}`} style={
+            <div className={`bta img ${this.props.className||""}`} onClick={genlink(this.props.click)} style={
                 (
                     ori_V?(
                         asi(ori_V_style, {
